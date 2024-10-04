e7e25cd73152db5bc86ee01426c4c7be803b3281
ocaocuoci <hernandar0606@gmail.com>
2024-10-05 06:22:16 +0700

    yyy

---
+++ JgnBodySwimming.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class JgnBodySwimming{
+    public static void main(String[] ergs) {
+        Scanner input12 = new Scanner(System.in);
+
+        Double BMI;
+        int tinggi, berat;
+
+        System.out.print("Masukkan berat badan anda (kg) : ");
+        berat = input12.nextInt();
+        System.out.print("Masukkan tinggi badan anda (cm) : ");
+        tinggi = input12.nextInt();
+
+        BMI = berat / tinggi;
+
+        if ( BMI < 18.5){
+            System.out.print("Anda termasuk berbadan kurus");
+        } else if ( BMI >= 18.5 && BMI <= 25){
+            System.out.print("Anda termasuk berbadan langsing/sehat");
+        } else if (BMI >= 25){
+            System.out.print("Anda termasuk berbadan gemuk");
+        } else {
+            System.out.print("tidak valid");
+        }
+
+        
+    }
+}
